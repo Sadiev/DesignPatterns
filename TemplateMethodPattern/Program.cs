bf90376b901be4afabbfa1c08ab48b03c8744429
Dilshod Sadiev <Dilshod.Sadiev@tibercreek.com>
2023-02-24 13:42:00 -0500

    Template Method Pattern

---
+++ TemplateMethodPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var task=new TransferMoneyTask();
+            task.execute();
+
+            var report=new GenerateReportTask();
+            report.execute();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TemplateMethodPattern/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern
+{
+    internal abstract class Task
+    {
+        private AuditTrail auditTrail;
+        public Task()
+        {
+            this.auditTrail = new AuditTrail();
+        }
+        public Task(AuditTrail auditTrail)
+        {
+            this.auditTrail = auditTrail;
+        }
+
+        public void execute()
+        {
+            auditTrail.record();
+            doExecute();
+        }
+        protected abstract void doExecute();
+    }
+}

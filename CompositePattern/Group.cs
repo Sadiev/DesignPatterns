4a7190f985ae781e7c1cd1641a6e9d3b960fff73
Dilshod Sadiev <Dilshod.Sadiev@tibercreek.com>
2023-03-03 17:25:56 -0500

    Composite Pattern

---
+++ CompositePattern/Group.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    internal class Group: IComponent
+    {
+        private List<IComponent> components = new List<IComponent>();
+        public void add(IComponent component) { components.Add(component); }
+
+        public void move()
+        {
+            foreach (IComponent component in components)
+            {
+                component.move();
+            }
+        }
+
+        public void render()
+        { 
+            foreach (IComponent component in components)
+            {
+                component.render();
+            }
+        } 
+    }
+}
---
+++ CompositePattern/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    internal class Shape : IComponent
+    {
+        public void move()
+        {
+            Console.WriteLine("Move Shape");
+
+        }
+
+        public void render()
+        {
+            Console.WriteLine("Render Shape");
+        }
+    }
+}
